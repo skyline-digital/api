fdb5276fd676d12ced896b1681574cf024cee115
jackdar <jackdarlington2001@gmail.com>
2024-11-15 01:29:24 +1300

    feat!: implement create functions including structs being passed

--- internal/store/posts.go
+++ internal/store/posts.go
@@ -3,12 +3,36 @@ package store
 import (
 	"context"
 	"database/sql"
+
+	"github.com/lib/pq"
+
+	"github.com/skyline-digital/api/internal/models"
 )
 
 type PostStore struct {
 	db *sql.DB
 }
 
-func (s *PostStore) Create(ctx context.Context) error {
+func (s *PostStore) Create(ctx context.Context, post *models.Post) error {
+	query := `
+		INSERT INTO posts (title, body, user_id, tags)
+		VALUES ($1, $2, $3, $4) RETURNING id, created_at, updated_at
+	`
+
+	if err := s.db.QueryRowContext(
+		ctx,
+		query,
+		post.Title,
+		post.Body,
+		post.UserID,
+		pq.Array(post.Tags),
+	).Scan(
+		&post.ID,
+		&post.CreatedAt,
+		&post.UpdatedAt,
+	); err != nil {
+		return err
+	}
+
 	return nil
 }
--- internal/store/storage.go
+++ internal/store/storage.go
@@ -3,14 +3,16 @@ package store
 import (
 	"context"
 	"database/sql"
+
+	"github.com/skyline-digital/api/internal/models"
 )
 
 type Storage struct {
 	Posts interface {
-		Create(context.Context) error
+		Create(context.Context, *models.Post) error
 	}
 	Users interface {
-		Create(context.Context) error
+		Create(context.Context, *models.User) error
 	}
 }
 
