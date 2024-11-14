e8253107898dbb3b18ba875a4f55e49993e9fbdb
jackdar <jackdarlington2001@gmail.com>
2024-11-15 01:48:56 +1300

    feat: allow parsing of durations from env vars

--- internal/env/env.go
+++ internal/env/env.go
@@ -3,6 +3,7 @@ package env
 import (
 	"os"
 	"strconv"
+	"time"
 )
 
 func GetString(key, fallback string) string {
@@ -26,4 +27,18 @@ func GetInt(key string, fallback int) int {
 	}
 
 	return valAsInt
+}
+
+func GetDuration(key string, fallback time.Duration) time.Duration {
+	val, ok := os.LookupEnv(key)
+	if !ok {
+		return fallback
+	}
+
+	valAsDuration, err := time.ParseDuration(val)
+	if err != nil {
+		return fallback
+	}
+
+	return valAsDuration
 }
